fe9adfb9b2e74e94f88820ac1f297ab0ceb0e490
p-herbert <pete.a.herbert@gmail.com>
2016-10-18 12:02:26 -0700

    (feat) Create class TicTacToe

---
+++ game.js
@@ -0,0 +1,36 @@
+const prompt = require('prompt');
+
+class TicTacToe = {
+  constructor(options) {
+    this.board = [['', '', ''], ['', '', ''], ['', '', '']];
+    this.playerOne = options.one;
+    this.playerTwo = options.two;
+  }
+
+  // Print the board
+  printBoard() {
+
+  }
+
+  // Mark the position on the board
+  draw(pos, mark) {
+    this.board[pos[0]][pos[1]] = mark;
+  }
+
+  // Determine if there is a winner
+  winner() {
+
+  }
+
+  // Check if position is empty
+  checkPos(pos) {
+    return this.board[pos[0]][pos[1]] === '';
+  }
+
+  // Check user input
+  checkInput(pos) {
+    return 0 <= pos[0] <= 2 && 0 <= pos[1] <= 2;
+  }
+}
+
+prompt.start();
